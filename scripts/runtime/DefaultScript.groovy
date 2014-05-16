948f4da58bf711f55521009bbbf18662fdf20d60
Paul Slusarz <PaulSlusarz@carfax.com>
2014-05-16 16:47:32 -0500

    term cloud for class names

--- scripts/runtime/DefaultScript.groovy
+++ scripts/runtime/DefaultScript.groovy
@@ -11,60 +11,12 @@ def isGenerated(ArcheologyFile file) {
     file.canonicalPath.contains('protobuf/generated') || 
     file.canonicalPath.contains('codegen')
 }
-
-
-
-
-
+//
+//
+//
+//
+//
 List<DataPoint3d> dp = []
-
-def moduleColors = [
-      'ant'          : 'Orange',
-      'camel'        : 'Red',
-      'cassandra'    : 'Cyan',
-      'hadoop-common': 'Blue',
-      'httpclient'   : 'Yellow'  
-    ]
-Map<String, List<ArcheologyFile>> wordsWithClasses = [:].withDefault {[]}
-modules*.files.flatten().findAll{it.javaName() && !it.javaName().startsWith('java') && 
-         !isGenerated(it)}.each { ArcheologyFile file ->
-         def words = NameParser.getWords(file.javaName()) 
-         words.each { String word ->
-             wordsWithClasses[word] << file
-         }
-         
-       
-}
-
-wordsWithClasses.sort {-it.value.size()}.eachWithIndex { String word, List<ArcheologyFile> files, int i ->
-    if (files.size() > 10) {
-        String usages = ""
-        files.each {
-          usages += "${it.javaName()}[${it.module.name}] , "  
-        }
-        println word + "  "+files.size()+"  "+ usages
-        dp << new DataPoint3d(name: word, x: i, y: 0, z:files.size())
-    }
-}
-
-return new DataPointProvider(xLabel: "rank",
-    yLabel: "",
-    zLabel: "occurences", dataPoints: dp)
-
-//
-////import org.sw7d.archeology.Module
-//import org.sw7d.archeology.ArcheologyFile
-//import org.sw7d.archeology.data.DataPointProvider
-//import org.sw7d.archeology.data.DataPoint3d
-//
-//
-//def isGenerated(ArcheologyFile file) {
-//    file.canonicalPath.contains('thrift') || 
-//    file.canonicalPath.contains('protobuf/generated') || 
-//    file.canonicalPath.contains('codegen')
-//}
-//
-//List<DataPoint3d> dp = []
 //
 //def moduleColors = [
 //      'ant'          : 'Orange',
@@ -72,41 +24,103 @@ return new DataPointProvider(xLabel: "rank",
 //      'cassandra'    : 'Cyan',
 //      'hadoop-common': 'Blue',
 //      'httpclient'   : 'Yellow'  
-//    ].withDefault {"DarkGray"}
+//    ]
+Map<String, List<ArcheologyFile>> wordsWithClasses = [:].withDefault {[]}
+modules*.files.flatten().findAll{it.javaName() && !it.javaName().startsWith('java') && 
+         !isGenerated(it)}.each { ArcheologyFile file ->
+         def words = NameParser.getWords(file.javaName()) 
+         words.each { String word ->
+             //String theWord = word.toLowerCase()[-1] == 's' ? word[0..-2].toLowerCase(): word.toLowerCase() 
+             wordsWithClasses[word.toLowerCase()] << file
+         }
+         
+       
+}
 //
-//import org.sw7d.archeology.Module
-//import org.sw7d.archeology.ArcheologyFile
-//import org.sw7d.archeology.data.DataPointProvider
-//import org.sw7d.archeology.data.DataPoint3d
-//
-//
-//
-//def today = new Date()
-// 
-//modules.each { Module module ->
-//    def javaFiles = module.files.findAll{it.javaName()}?:[]
-//    if (!javaFiles) {
-//        println "no java files, probably development on non standard branches: "+module.name
+//wordsWithClasses.sort {-it.value.size()}.eachWithIndex { String word, List<ArcheologyFile> files, int i ->
+//    if (files.size() > 10) {
+//        String usages = ""
+//        files.each {
+//          usages += "${it.javaName()}[${it.module.name}] , "  
+//        }
+//        println word + "  "+files.size()+"  "+ usages
+//        dp << new DataPoint3d(name: word, x: i, y: 0, z:files.size())
 //    }
-//    int totalCommits = javaFiles.sum {it.commits.size()} ?: 0
-//    int commitsPerJavaFile = totalCommits * 5 / (javaFiles.size() ?: 1)
-//    Date firstCommit = module.files*.commits.collect().flatten().min() ?: new Date() - 100000
-//    Date lastCommit = module.files*.commits.collect().flatten().max() ?: new Date() - 10000
-////    String color = "DarkGray" // 2+ years
-////    Date today = new Date()
-////    if (today - lastCommit <  180) {  // 1/2 year
-////        color = "Red"
-////    } else if (today - lastCommit  < 360) { // 1 year
-////        color = "Orange"
-////    } 
-////    else if (today - lastCommit < 720) {  // 2 years
-////        color = "Yellow"
-////    } 
-//
-//    dp << new DataPoint3d(name: module.name, 
-//        x: (today -firstCommit) / 70, 
-//        y: commitsPerJavaFile, type: "box", 
-//        z: module.files.findAll{it.javaName()}.size() / 100,  
-//        color: moduleColors[module.name], size:2)  
 //}
-//return new DataPointProvider(xLabel: "Age", yLabel: "Commits per file", zLabel: "java classes", dataPoints: dp)
+//
+
+
+String contents = """
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="http://visapi-gadgets.googlecode.com/svn/trunk/termcloud/tc.css"/>
+    <script type="text/javascript" src="http://visapi-gadgets.googlecode.com/svn/trunk/termcloud/tc.js"></script>
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+  </head>
+  <body>
+    <div id="tcdiv"></div>
+    <script type="text/javascript">
+      google.load("visualization", "1");
+      google.setOnLoadCallback(draw);
+      function draw() {
+        data = new google.visualization.DataTable();
+        data.addColumn('string', 'Label');
+        data.addColumn('number', 'Value');
+        data.addColumn('string', 'Link');
+"""
+def selectedWords = wordsWithClasses.findAll {it.value.size() > 100}.sort()
+contents += "                data.addRows(${selectedWords.size()});\n"
+selectedWords.eachWithIndex { String word, List<ArcheologyFile> files, int i ->
+    contents += "                    data.setValue(${i},0,'${word}');\n" 
+    contents += "                    data.setValue(${i},1,${files.size()});\n" 
+    contents += "                    data.setValue(${i},2,'${word}.html');\n"
+    File wordFile = new File("./results/termcloud/${word}.html")
+    wordFile.delete()
+    wordFile.parentFile.mkdirs()
+    wordFile << "<html><head><title>${word} in class names</title></head><body><h1>${files.size()} occurences of '${word}' in class names</h1>\n"
+    wordFile << "<p><a href='index.html'> &lt;&lt; Index</a></p>\n"
+    wordFile << "<table>\n"
+    files.groupBy {it.module.name}.sort().each { String moduleName, List<ArcheologyFile> moduleFiles -> 
+       wordFile << "   <tr>\n" 
+       wordFile << "      <td>${moduleName}</td>\n"
+       wordFile << "      <td>\n"
+       moduleFiles.each {
+           wordFile << "                ${it.javaName()}, \n"
+       }
+       wordFile << "      </td>\n"
+       wordFile << "   </tr>\n"  
+    }
+    wordFile << "</table>\n"
+    wordFile << "<p><a href='index.html'> &lt;&lt; Index</a></p>\n"
+    wordFile << "</body></html>\n"
+}
+
+//"        data.addRows(3);
+//        data.setValue(0, 0, 'First Term');
+//        data.setValue(0, 1, 10);
+//        data.setValue(1, 0, 'Second');
+//        data.setValue(1, 1, 30);
+//        data.setValue(1, 2, 'http://www.google.com');
+//        data.setValue(2, 0, 'Third');
+//        data.setValue(2, 1, 20);
+
+contents +=
+"""        var outputDiv = document.getElementById('tcdiv');
+        var tc = new TermCloud(outputDiv);
+        tc.draw(data, null);
+      }
+    </script>
+  </body>
+</html>
+
+"""
+
+File output = new File("./results/termcloud/index.html")
+output.delete()
+output.parentFile.mkdirs()
+output << contents
+"cmd /c start ${output.canonicalPath}".execute()
+
+return new DataPointProvider(xLabel: "rank",
+    yLabel: "",
+    zLabel: "occurences", dataPoints: dp)
