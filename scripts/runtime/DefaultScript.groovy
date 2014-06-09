99f3da210946b7d408b42007e0c2b17da7b21841
Pawel Slusarz <p@sw7d.com>
2014-06-09 00:55:13 -0500

    graph autoreports hierarchy based on groovydoc

--- scripts/runtime/DefaultScript.groovy
+++ scripts/runtime/DefaultScript.groovy
@@ -15,68 +15,59 @@ import org.codehaus.groovy.tools.groovydoc.gstringTemplates.GroovyDocTemplateInf
 import org.sw7d.archeology.Settings
 
 
-def isGenerated(ArcheologyFile file) {
-    file.canonicalPath.contains('thrift') || 
-    file.canonicalPath.contains('protobuf/generated') || 
-    file.canonicalPath.contains('codegen')
-}
-
 List<DataPoint3d> dp = []
 
 Set<String> paths = new HashSet<String>()
 List<String> files = []
 
-modules*.files.flatten().findAll{it.javaName() && !it.javaName().startsWith('java') && 
-         !isGenerated(it) && !(it.canonicalPath.contains('/test/') || it.canonicalPath.contains('/integration/'))}.each { ArcheologyFile file ->
+modules*.files.flatten().findAll{it.javaName() && !it.javaName().startsWith('java') && !(it.canonicalPath.contains('/test/') || it.canonicalPath.contains('/integration/'))}.each { ArcheologyFile file ->
          String packagePath = file.javaName().replaceAll(/\./, "/")+"."+file.extension()
-         //println (file.canonicalPath - packagePath)
          paths << file.canonicalPath - packagePath 
          files << packagePath
     dp << new DataPoint3d(z: file.linesCount, x: file.popularity, y: file.commits.size(), name: file.name)           
 }
 
-
-
-//Module ant = modules.find {it.name == 'ant'}
-//ant.path = new File(ant.path).canonicalPath.replaceAll("apache-data","archeology3d-data/apache")
-//println ant.path
-String common = "/Users/ps/sandbox-jmonkey/archeology3d-data/autoreports/stash/"
-
 GroovyDocTool plainTool = new GroovyDocTool(paths.toArray() as String[]);
-//
-//
-//List<String> files = []
-//
-//modules.find {it.name == 'ant'}.files.findAll{it.javaName()}.each {
-//    files << it.javaName().replaceAll(/\./, "/")+"."+it.extension()
-//}
-//
 plainTool.add (files)
-//
 
 Map<String,List<GroovyClassDoc>> interfaceImplementations = [:].withDefault{[]}
 
 GroovyRootDoc root = plainTool.getRootDoc();
 GroovyClassDoc[] classDocs = root.classes();
 classDocs.each { GroovyClassDoc clazz ->
-//    println "========================="
-//    println clazz.qualifiedTypeName()
-//    println "  interfaces: "+clazz.interfaces()
     clazz.interfaces().each {
       interfaceImplementations[it.qualifiedTypeName()] << clazz        
     }
-//    println "  superclass: "+  clazz.superclass()?.name()
-//    println "  imports: "+clazz.importedClasses()
-//    println "  methods: "+clazz.methods()
-//    println "  imported packages"+clazz.importedPackages()
-//    GroovyMethodDoc[] methodDocs = clazz.methods();
-//    println ""
 }
 
 interfaceImplementations.sort{it.value.size()}.each { key, value ->
     println key+ "  "+value.size()
     println "    "+value.collect {it.qualifiedTypeName()}
 }
+
+String graphName = "autoreports"
+
+String output = """digraph ${graphName} {
+    rankdir=LR;
+"""
+
+classDocs.each { GroovyClassDoc clazz ->
+    clazz.interfaces().each { GroovyClassDoc iface ->
+       if (iface.qualifiedTypeName().contains("carfax")) {
+         output += "  ${clazz.name().replaceAll(/\./,'_')} -> ${iface.name()} [color=\"red\"]; \n"    
+       }
+    }
+    if (clazz.superclass() && clazz.superclass().qualifiedTypeName().contains("carfax")) {
+        output += "  ${clazz.name().replaceAll(/\./,'_')} -> ${clazz.superclass().name()}; \n"
+    }
+}
+output += "}"
+println output
+def file = new File("graphviz.in")
+file.delete()
+file.write(output)
+"/usr/local/bin/dot -o${graphName}.png -Tpng graphviz.in".execute().waitFor()
+"open ${graphName}.png".execute()
 
 
 
