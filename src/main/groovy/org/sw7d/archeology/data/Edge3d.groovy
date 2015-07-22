e62ccd13d28b09a87aa80be429527552f9cb5aca
Pawel Slusarz <p@sw7d.com>
2015-07-22 00:20:02 -0500

    git interface in jmonkey is silly and did not add this file on previous commit

---
+++ src/main/groovy/org/sw7d/archeology/data/Edge3d.groovy
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sw7d.archeology.data
+
+/**
+ *
+ * @author ps
+ */
+class Edge3d {
+    DataPoint3d from, to
+	
+}
+
